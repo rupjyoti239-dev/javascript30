24ce180f093a7e66aaec1ee1ef8b9225e5fbbcfe
Rupjyoti Sarma <rup.sarma23@gmail.com>
2024-08-24 01:23:58 +0530

    day 1 completed

---
+++ Day 1/index.js
@@ -0,0 +1,54 @@
+let name = "Rupjyoti";
+let age = 24;
+let isMarried = false;
+let currentSalary ;
+let previousSalary = 50.50;
+let title = null;
+
+console.log(
+  `name: ${name},
+   age: ${age},
+   isMarried: ${isMarried},
+  currentSalary: ${currentSalary},
+  previousSalary: ${previousSalary},
+  title: ${title},
+  `
+);
+
+
+
+console.log(
+ `
+   typeof name: ${typeof name},
+   typeof isMarried: ${typeof isMarried},
+   typeof currentSalary: ${typeof currentSalary},
+   typeof previousSalary: ${typeof previousSalary},
+   typeof title: ${typeof title},
+ ` 
+);
+
+
+
+
+let myAge = 24;
+let yourAge = 20;
+console.log(
+  `
+  My age is ${myAge} and Your age is ${yourAge}
+  `
+);
+
+
+
+{
+  let x = 10;
+  console.log(x);
+  
+}
+
+{
+  let x = 20;
+  console.log(x);
+  
+}
+
